9ac90e3c98ef1db300892a9bfe8e9ee0a27a1052
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2022-05-15 09:55:20 +0600

    Update nodejs-crud-operation.js

--- nodejs-crud-operation.js
+++ nodejs-crud-operation.js
@@ -1,3 +1,17 @@
+```
+  /**
+     * API Naming Convention
+     * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+     * app.get('/booking/:id') // get a specific booking 
+     * app.post('/booking') // add a new booking
+     * app.patch('/booking/:id) //
+     * app.delete('/booking/:id) //
+    */
+```
+
+
+
+
 //update,post,delete,load data
 
 const express = require("express");
