c129bee15a92a9f46f548c303c1b40132c441592
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2022-05-21 09:47:03 +0600

    Create mongodb data put.js

---
+++ mongodb data put.js
@@ -0,0 +1,43 @@
+    // User Creation and update
+    app.put("user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+    });
+
+
+
+import { useEffect, useState } from "react";
+
+const useToken = (user) => {
+  const [token, setToken] = useState("");
+  
+  useEffect(() => {
+    const email = user?.user?.email;
+    const currentUser = { email: email };
+    if (email) {
+      fetch(`http://localhost:5000/user/${email}`, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(currentUser),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("data inside useToken", data);
+          const accessToken = data.token;
+          localStorage.setItem("accessToken", accessToken);
+          setToken(accessToken);
+        });
+    }
+  }, [user]);
+  return [token];
+};
+
+export default useToken;
